2535de3d4eb5cbe4ad9d97451885afcc6517fe6e
swearingen-ben <benjamin.swearingen@mail.mcgill.ca>
2016-07-11 22:02:09 -0400

    Dom walker from http://stackoverflow.com/questions/5904914/

--- content.js
+++ content.js
@@ -1,1 +1,29 @@
-document.body.innerHTML = document.body.innerHTML.replace(/notallmen/gi, 'NoTallMen');
+// Walker structure taken from T.J. Crowder:              
+// http://stackoverflow.com/questions/5904914/
+
+walk(document.body);
+
+// Our walker function
+function walk(node) {
+  var child, next;
+
+  switch (node.nodeType) {
+    case 1:  // Element
+    case 9:  // Document
+    case 11: // Document fragment
+      child = node.firstChild;
+      while (child) {
+        next = child.nextSibling;
+        walk(child);
+        child = next;
+      }
+      break;
+    case 3: // Text node
+      handleText(node);
+      break;
+  }
+}
+
+function handleText(textNode) {
+  textNode.nodeValue = textNode.nodeValue.replace(/notallmen/gi, "NoTallMen");
+}
